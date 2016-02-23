78215c9a7aa63034dfabc620676a593dd2501de6
samsars10 <sarstens@gmail.com>
2016-02-22 22:39:54 -0800

    First Commit

---
+++ MainController.js
@@ -0,0 +1,16 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.tracks = [
+    {
+      name: '1. Intro',
+      length: 0:45
+    },
+    {
+      name: '2. Tinder Success Story',
+      length: 4:10
+    },
+    {
+      name: "3. Lola's Jam (Bonus Track)",
+      length: 3:15
+    }
+  ];
+}]);
---
+++ app.js
@@ -0,0 +1,15 @@
+var app = angular.module ("TrackListApp",[]);
+
+var main = function () {
+
+
+
+
+
+
+};
+
+
+$(document).ready(main);
+
+// This is all useless
